86d986016ac4aa75ed1ce340ce19b280623ee1ac
fullnull <fullnull@163.com>
2014-07-15 10:38:30 +0800

    Format

--- src/main/java/com/ruyicai/weixin/consts/ResponseMsgType.java
+++ src/main/java/com/ruyicai/weixin/consts/ResponseMsgType.java
@@ -1,7 +1,11 @@
 package com.ruyicai.weixin.consts;
 
 public class ResponseMsgType {	
+	
 	public static String TEXT = "text";
+	
 	public static String MUSIC = "music";
+	
 	public static String NEWS = "news";	
+	
 }
