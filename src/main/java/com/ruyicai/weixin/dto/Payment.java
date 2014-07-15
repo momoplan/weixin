f91d62e7682517e70370aeb84d9ac0de28e1e5bd
fullnull <fullnull@163.com>
2014-07-15 17:59:36 +0800

    增加两个属性

--- src/main/java/com/ruyicai/weixin/dto/Payment.java
+++ src/main/java/com/ruyicai/weixin/dto/Payment.java
@@ -10,7 +10,30 @@ import javax.xml.bind.annotation.XmlAccessorType;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Payment {
 	
+	//交易金额
 	private String TotalFee;
+	
+	//商品描述
+	private String Body;
+	
+	//用户浏览器端 IP
+	private String BillCreateIP;
+
+	public String getBillCreateIP() {
+		return BillCreateIP;
+	}
+
+	public void setBillCreateIP(String billCreateIP) {
+		BillCreateIP = billCreateIP;
+	}
+
+	public String getBody() {
+		return Body;
+	}
+
+	public void setBody(String body) {
+		Body = body;
+	}
 
 	public String getTotalFee() {
 		return TotalFee;
