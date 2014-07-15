fae5be1861c0b2d2f27df5896ba572b9446983bd
fullnull <fullnull@163.com>
2014-07-15 10:22:49 +0800

    Change Format

--- src/main/java/com/ruyicai/weixin/consts/ResponseMsgType.java
+++ src/main/java/com/ruyicai/weixin/consts/ResponseMsgType.java
@@ -1,7 +1,9 @@
 package com.ruyicai.weixin.consts;
 
 public class ResponseMsgType {
+	
 	public static String TEXT = "text";
 	public static String MUSIC = "music";
 	public static String NEWS = "news";	
+	
 }
