32f016c6e7423e7796691e0cee3c98caa4a270e9
fullnull <fullnull@163.com>
2014-08-21 14:49:03 +0800

    生成JA

--- src/main/java/com/ruyicai/weixin/domain/Packet.java
+++ src/main/java/com/ruyicai/weixin/domain/Packet.java
@@ -1,47 +1,12 @@
 package com.ruyicai.weixin.domain;
-
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import org.springframework.roo.addon.dbre.RooDbManaged;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
-import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
-@RooJpaActiveRecord
-@RooJpaEntity(table = "packet", versionField = "")
+@RooJpaActiveRecord(versionField = "", table = "packet")
+@RooDbManaged(automaticallyDelete = true)
 public class Packet {
-
-	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-
-	@Column(name = "openid")
-	private String openid;
-
-	@Column(name = "packet_userno")
-	private String packet_userno;
-
-	@Column(name = "total_persons")
-	private int total_persons;
-
-	@Column(name = "total_punts")
-	private int total_punts;
-
-	@Column(name = "real_parts")
-	private int real_parts;
-
-	@Column(name = "greetings")
-	private String greetings;
-
-	@Column(name = "createtime")
-	private Date createtime;
-
 }
--- src/main/java/com/ruyicai/weixin/domain/PuntPacket.java
+++ src/main/java/com/ruyicai/weixin/domain/PuntPacket.java
@@ -1,44 +1,12 @@
 package com.ruyicai.weixin.domain;
-
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import org.springframework.roo.addon.dbre.RooDbManaged;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
-import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
-@RooJpaActiveRecord
-@RooJpaEntity(table = "punt_packet", versionField = "")
+@RooJpaActiveRecord(versionField = "", table = "punt_packet")
+@RooDbManaged(automaticallyDelete = true)
 public class PuntPacket {
-
-	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-
-	@Column(name = "packet_id")
-	private int packet_id;
-
-	@Column(name = "random_punts")
-	private int random_punts;
-
-	@Column(name = "openid")
-	private String openid;
-
-	@Column(name = "get_userno")
-	private String get_userno;
-
-	@Column(name = "get_time")
-	private Date get_time;
-
-	@Column(name = "thank_words")
-	private String thank_words;
-
 }
